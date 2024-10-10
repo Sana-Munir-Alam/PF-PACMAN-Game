ba9e654fc59ce9967972228beeb1b3afb2b7fcdc
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2024-10-10 09:55:24 +0500

    Update titile.c

--- titile.c
+++ titile.c
@@ -6,8 +6,12 @@ int main(){
     printf("| |_) / _ \\| |   | |\\/| | / _ \\ |  \\| |\n");
     printf("|  __/ ___ | |___| |  | |/ ___ \\| |\\  |\n");
     printf("|_| /_/   \\_\\____|_|  |_/_/   \\_|_| \\_|\n");
-
-    printf("INSTRUCTIONS:");
-
+    printf("\n\n\n");
+    printf("INSTRUCTIONS:\n");
+    printf("To Move Up Press W\n");
+    printf("To Move Down Press S\n");
+    printf("To Move Right Press D\n");
+    printf("To Move Left press A\n");
+    printf("\n\n");
     return 0;
 }
