db5355713cf04a2aba7d650f972c51a932e7d99f
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2024-11-02 19:16:33 +0500

    Update 3) Initialise-Game.c

--- PACMAN_for_MACOS/3) Initialise-Game.c
+++ PACMAN_for_MACOS/3) Initialise-Game.c
@@ -2,20 +2,26 @@
 
 // Initializes or resets the game variables and map
 void InitializeGame(char Map[Height][Width], int *x, int *y, int *Lives, bool resetScore, bool resetHighScore) {
-    // Reset player position, lives, and score
+    // Reset PACMAN position, lives, and score
     *x = 29;
     *y = 17;
     *Lives = 3;
     Score = resetHighScore? 0 : 0;
     HighScore = resetHighScore ? 0 : HighScore;
 
-    // Reset the map with coins and clear previous player positions
+    // Reset the map with coins and clear previous characters positions
     for (int i = 0; i < Height; ++i) {
         for (int j = 0; j < Width; ++j) {
             if (i == 17 && j == 29) {
                 Map[i][j] = Pacman;
             } else if (i == 5 && j == 14) {
                 Map[i][j] = GhostGang;
+            } else if ( i == 11 && j == 23){
+                Map[i][j] = GhostGang2;
+            } else if (i == 19 && j ==45){
+                Map[i][j] = GhostGang3;
+            } else if(i == 1 && j == 14){
+                Map[i][j] = GhostGang4;
             } else {
                 Map[i][j] = Coins;
             }
