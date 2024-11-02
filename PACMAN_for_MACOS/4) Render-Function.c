a3e22182524fc5ce039a40fa8e6a3bd817b130e6
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2024-11-02 19:17:00 +0500

    Update 4) Render-Function.c

--- PACMAN_for_MACOS/4) Render-Function.c
+++ PACMAN_for_MACOS/4) Render-Function.c
@@ -1,8 +1,9 @@
 // Section 4: This function is called every iteration to update the maze state as per character movement, it also displays many important things (like Lives status, Score, High Score, Time Elapsed etc)
 
 // Render the box and character (every iteration)
-void Render(int x, int y, int Ghost1X, int Ghost1Y, int Lives, int Score, int HighScore, double ElapsedTime, char Map[Height][Width]) {
+void Render(int x, int y, int Ghost1X, int Ghost1Y, int Ghost2X, int Ghost2Y, int Ghost3X, int Ghost3Y, int Ghost4X, int Ghost4Y, int Lives, int Score, int HighScore, double ElapsedTime, char Map[Height][Width]) {
     move_cursor(0, 0);  // Move cursor to the top left corner
+    
     // Draw the Maze and Characters
     for (int i = 0; i < Height; ++i) {
         for (int j = 0; j < Width; ++j) {
@@ -11,8 +12,14 @@ void Render(int x, int y, int Ghost1X, int Ghost1Y, int Lives, int Score, int Hi
             } else if (i == y && j == x) {
                 printf("%c", Pacman);  // The Pacman
             } else if (i == Ghost1Y && j == Ghost1X) {
-                printf("%c", GhostGang);  // Ghost1
-            } else if (Map[i][j] == '.') {
+                printf("%c", GhostGang);    // Ghost1
+            } else if (i == Ghost2Y && j == Ghost2X){
+                printf("%c", GhostGang2);   // Ghost2
+            } else if (i == Ghost3Y && j == Ghost3X){
+                printf("%c", GhostGang3);   // Ghost3
+            } else if (i == Ghost4Y && j == Ghost4X){
+                printf("%c", GhostGang4);   // Ghost4
+            }else if (Map[i][j] == '.') {
                 printf("%c", Coins);  // Coin
             } else {
                 printf("%c", Empty);  // Empty space
@@ -21,10 +28,13 @@ void Render(int x, int y, int Ghost1X, int Ghost1Y, int Lives, int Score, int Hi
         printf("\n");
     }
     printf("Lives: %d\n", Lives); // Display Remaining Lives
-    printf("Score: %d\n", Score); // Display the Score
+    printf("Score: %d\n", Score); // Display the Player Score
     printf("High Score: %d\n", HighScore); // Display the High Score
     printf("Time: %.0f seconds\n", ElapsedTime); // Display Elapsed Time
-    printf("Pacman(%d,%d)", x,y);
-    printf(" Demon (%d,%d)", Ghost1X, Ghost1Y);
+    printf("Pacman(%d,%d)\n", x,y);
+    printf("Demon 1 (%d,%d)\n", Ghost1X, Ghost1Y);
+    printf("Demon 2 (%d,%d)\n", Ghost2X, Ghost2Y);
+    printf("Demon 3 (%d,%d)\n", Ghost3X, Ghost3Y);
+    printf("Demon 4 (%d,%d)\n", Ghost4X, Ghost4Y);
     fflush(stdout);  // Ensure everything is printed immediately
 }
