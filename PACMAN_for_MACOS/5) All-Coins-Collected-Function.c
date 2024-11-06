d394c0ddf31dcc0dd36273fc25c7ac5e407f70e3
Sana-Munir-Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2024-11-06 07:41:12 -0800

    Update 5) All-Coins-Collected-Function.c

--- PACMAN_for_MACOS/5) All-Coins-Collected-Function.c
+++ PACMAN_for_MACOS/5) All-Coins-Collected-Function.c
@@ -1,8 +1,8 @@
-Section 5: This function checks whether all the coins have been collected so that it can return true to indicate the Game Over Scene
+// Section 5: This function checks whether all the coins have been collected so that it can return true to indicate the Game Over Scene
 
 // Function to check if all coins have been collected
-bool AllCoinsCollected(int Coin) {
-    if (Coin != 615){
+bool AllCoinsCollected(int Food) {
+    if (Food != 613){
         return false;   // Coins Left
     }
     return true;  // No coins left
