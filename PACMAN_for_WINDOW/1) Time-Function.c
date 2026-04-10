ac1f6fcf4f6fc9706865c1b2a870132e12eee1ea
Sana Munir Alam <145678555+Sana-Munir-Alam@users.noreply.github.com>
2026-04-10 16:28:31 +0500

    Delete PACMAN_for_WINDOW directory

--- PACMAN_for_WINDOW/1) Time-Function.c
+++
@@ -1,6 +0,0 @@
-//Section 1: This Function handles the time calculation (from when the game begins till the game ends)
-
-// Time Function
-double GetElapsedTime(time_t StartTime) {
-    return difftime(time(NULL), StartTime);
-}
